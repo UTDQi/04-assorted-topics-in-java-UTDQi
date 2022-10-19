6070b508d043ed20df6c56fee0f2f56b9d56a691
UTDQi <113342231+UTDQi@users.noreply.github.com>
2022-10-19 15:54:39 -0400

    Finished TODOs

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -1,13 +1,24 @@
-/* TODO: Create a subclass of Trader named DrivableTrader
- * This class should be identical to Trader, except that it takes
- * only Drivable objects in its inventory, wishlist, etc.
- *
- * The sellingPrice returned should also be overridden. The selling price
- * should be equal to the:
- *     Object's price + Object's max speed
- * If the object is Tradable (and Tradable.MISSING_PRICE otherwise.)
- *
- * Look at DomesticatableTrader.java for an example.
- */
 
 import java.util.List;
+
+public class DrivableTrader extends Trader<Drivable>{
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable object){
+        if(object.getClass() == Tradable){
+            return ((Tradable)object).getPrice()+ object.getMaxSpeed();
+        }else{
+            return Tradable.MISSING_PRICE;
+        }
+    }
+
+
+}
